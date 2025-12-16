f5253ef020d9d8836d72c392a906a679abd4f2a7
wnsgh7368 <sin010820@gmail.com>
2025-12-16 14:00:28 +0900

    setting: db연결 및 환경변수 설정

---
+++ prisma/migrations/20251216044845_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(20) NOT NULL,
+    `userReferenceId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `email`(`email`),
+    UNIQUE INDEX `User_userReferenceId_key`(`userReferenceId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
